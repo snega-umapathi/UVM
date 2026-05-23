c37f929ea74d8ba852b47a00ab8a17018a1e65cb
snega-umapathi <snegaumapathi@gmail.com>
2026-05-23 17:26:39 +0530

    Update dut.sv

--- dut.sv
+++ dut.sv
@@ -7,16 +7,17 @@ endmodule
 //RNF behaviour
 module rnf
   input endpoint_addr; //this should be driven by the uvm?
-  reg txreq;
+  output reg txreq;
+  input reg txrsp;
   output wire READREQ;
   output wire WRITEREQ;
   case(reqtype)
     writereq: begin
       assign WRITEREQ = 1;
       //need to send a write transaction: so first need to send a TXREQ
-      reg txreq = 1;
+      txreq = 1;
       #5 if 
-  end
+      end
   readreq: begin
     assign READREQ = 1;
   end 
